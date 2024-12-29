e068d5a21110158a28411d3ac655945638bb8e11
Mayank Roy <mayankroy106106@gmail.com>
2024-12-29 17:11:09 +0530

    Add button and update layout in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import VideoBg from '../src/video/Mahindra.mp4'
+import  ArrowRight  from "../src/assets/right-arrow.svg";
+
 
 function App() {
   return (
     <>
      
     <div >
-    <nav className="flex mx-5 mt-4 fixed text-white z-10"> 
-      <span className="logo">logo</span>
+    <nav className="flex mx-5 mt-4 absolute text-white z-10"> 
       <ul className="flex left-full relative space-x-11 font-serif text-2xl">
       <li className="hover:underline"><a href="#home">Home</a></li>
       <li className="hover:underline"><a href="#about">About</a></li>
@@ -14,8 +15,17 @@ function App() {
       <li className="hover:underline"><a href="#contact">Contact</a></li>
       </ul>
      </nav>
-      <video  src={VideoBg} autoPlay loop  className="w-full h-screen object-cover" ></video>
-      
+     <div className='fixed text-white  top-3/4 left-3/4  z-10'>
+     <button className="group">
+      Button
+      {/* <img src= {ArrowRight} alt="" className="-me-1 ms-2  opacity-60 transition-transform group-hover:translate-x-0.5"
+        size={16}
+        strokeWidth={2}
+        aria-hidden="true" /> */}
+    </button>
+      </div>
+      <video  src={VideoBg} autoPlay loop  className="w-full h-screen object-cover" >
+      </video>
      </div>
     </>
     )
